535daea9f62b1a83c7b8fcec6285032eeb75d39f
MMMMins <tommp1055@gmail.com>
2023-04-06 16:00:37 +0900

    upload Question K

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # 내가 푼 문제 업로드
 
 ## [2023.04.06]Upload
-- ### [K번째 수][문제링크] | [코드보기][코드링크]
+- ### [K번째 수][K번째 수] | [코드보기][K번째 수_코드링크]
+- ### [체육복][체육복] | [코드보가][체육복_코드링크]
 
+[K번째 수]: https://school.programmers.co.kr/learn/courses/30/lessons/42748
+[K번째 수_코드링크]: https://github.com/MMMMins/Java_Question/blob/main/src/K%EB%B2%88%EC%A7%B8%EC%88%98/Question.java
 
-[문제링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42748
-[코드링크]: https://github.com/MMMMins/Java_Question/blob/main/src/K%EB%B2%88%EC%A7%B8%EC%88%98/Question.java
+[체육복]: https://school.programmers.co.kr/learn/courses/30/lessons/42862
+[체육복_코드링크]
---
+++ src/체육복/Question.java
@@ -0,0 +1,55 @@
+package 체육복;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Question {
+    public static void main(String[] args) {
+        System.out.println(solution(5, new int[]{2, 4}, new int[]{1, 3, 5}));
+    }
+
+    public static int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+
+        // 1. 1차 에러 문제점 해결
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+        //초기화
+        HashMap<Integer, Integer> stu = new HashMap<>();
+        for(int i=1; i<=n; i++){
+            stu.put(i,1);
+        }
+
+        //도난당한 학생
+        for (int key : lost) {
+            stu.put(key, stu.get(key) - 1);
+        }
+
+        //여분있는 학생
+        for (int key : reserve) {
+            stu.put(key, stu.get(key) + 1);
+        }
+
+        // 2. 1차 에러 문제점 해결
+        for(int k : stu.keySet()){
+            if(stu.get(k) == 0){
+                if(k > 1 && stu.getOrDefault(k-1,0) == 2){
+                    stu.put(k-1, stu.get(k-1)-1);
+                    stu.put(k, stu.get(k)+1);
+                }else if(k != n+1 && stu.getOrDefault(k+1, 0) == 2){
+                    stu.put(k+1, stu.get(k+1)-1);
+                    stu.put(k, stu.get(k)+1);
+                }
+            }
+        }
+
+        for(int k : stu.keySet()){
+            if(stu.get(k) >= 1){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
